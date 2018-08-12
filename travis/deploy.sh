e1a7d3d80816ab7854704bcbeb2460d51c8ea550
Travis CI <lukas@connectservers.net>
2018-08-12 19:34:53 -0400

    travis.yml

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -59,7 +59,7 @@ mv docs/docs.json out/$SOURCE.json
 cd out
 git add .
 git config user.name "Travis CI"
-git config user.email "lukas@connectservers.net"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 git commit -m "Docs build for ${SOURCE_TYPE} ${SOURCE}: ${SHA}" || true
 git push $SSH_REPO $TARGET_BRANCH
 
@@ -78,6 +78,6 @@ mv webpack/discord.min.js out/discord.$SOURCE.min.js
 cd out
 git add .
 git config user.name "Travis CI"
-git config user.email "lukas@connectservers.net"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 git commit -m "Webpack build for ${SOURCE_TYPE} ${SOURCE}: ${SHA}" || true
 git push $SSH_REPO $TARGET_BRANCH
